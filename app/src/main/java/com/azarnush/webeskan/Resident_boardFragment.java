b10fb7d1e33eb42ecbf438ceae5c52e0a8c298ca
mary azarnush <mary136804@gmail.com>
2020-07-13 18:59:31 +0430

    sherkat 4/23 dashboard ok

--- app/src/main/java/com/azarnush/webeskan/Resident_boardFragment.java
+++ app/src/main/java/com/azarnush/webeskan/Resident_boardFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -17,17 +19,21 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.azarnush.webeskan.Adapter.UnitsAdapter;
+import com.google.gson.JsonObject;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Resident_boardFragment extends Fragment {
-
+    View root;
+    public TextView total_debt, total_Credit, announcements, your_credit, the_latest_payments, debts;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_resident_board, container, false);
-
+        root = inflater.inflate(R.layout.fragment_resident_board, container, false);
+        initViews();
         Resident_panelActivity.toolbar.setTitle("پنل ساکن");
         Toast.makeText(getContext(), UnitsAdapter.residenceRefId.toString(), Toast.LENGTH_LONG).show();
         new Handler().post(new Runnable() {
@@ -36,8 +42,18 @@ public class Resident_boardFragment extends Fragment {
                 sendJsonObjectRequest_getResidenceDashboard();
             }
         });
-
         return root;
+    }
+
+    private void initViews() {
+
+        total_debt = root.findViewById(R.id.total_debt);
+        total_Credit = root.findViewById(R.id.total_Credit);
+        announcements = root.findViewById(R.id.announcements);
+        your_credit = root.findViewById(R.id.your_credit);
+        the_latest_payments = root.findViewById(R.id.the_latest_payments);
+        debts = root.findViewById(R.id.debts);
+
     }
 
     @Override
@@ -48,24 +64,34 @@ public class Resident_boardFragment extends Fragment {
 
     public void sendJsonObjectRequest_getResidenceDashboard() {
         RequestQueue queue = Volley.newRequestQueue(getContext());
-        String url = "http://api.webeskan.com/api/v1/residence/dashboard/" + UnitsAdapter.residenceRefId;
+        ////+ UnitsAdapter.residenceRefId
+        String url = "http://api.webeskan.com/api/v1/residence/dashboard/93";
 
         Response.Listener<JSONObject> listener = new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
+                try {
+                    String s = "";
+                    JSONArray jsonArray = response.getJSONArray("debtList");
+                    Log.i("ptm", jsonArray.get(0).toString());
 
-
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        s = s + (jsonObject.getInt("debtAmount")) + "\n";
+                    }
+                    debts.setText(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 Toast.makeText(getContext(), response.toString(), Toast.LENGTH_LONG).show();
-
+                Log.i("ptm", response.toString());
             }
         };
 
         Response.ErrorListener errorListener = new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
-
                 Toast.makeText(getContext(), error.toString(), Toast.LENGTH_LONG).show();
             }
         };
