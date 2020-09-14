9bc3ff20df16e4d91f15d55ef1e3f95e21188deb
mary azarnush <mary136804@gmail.com>
2020-09-14 19:01:25 +0430

    ok edit

--- app/src/main/java/com/azarnush/webeskan/SplashActivity.java
+++ app/src/main/java/com/azarnush/webeskan/SplashActivity.java
@@ -1,33 +1,82 @@
 package com.azarnush.webeskan;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.azarnush.webeskan.App.AppController;
 import com.bumptech.glide.Glide;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    AlertDialog alert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
-        ImageView imageView = findViewById(R.id.gif);
+        imageView = findViewById(R.id.gif);
         Glide.with(this).load(R.drawable.splash).into(imageView);
 
+        sendversion();
+    }
+
+    public void sendversion() {
+        RequestQueue queve = Volley.newRequestQueue(getApplicationContext());
+        String versionname = String.valueOf(BuildConfig.VERSION_NAME);
+        String url = "http://api.webeskan.com/api/v1/users/get-version/" + versionname;
+        Response.Listener<String> listener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                if (response.equals("true")) {
+                    start();
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SplashActivity.this);
+                    builder.setTitle(R.string.app_name);
+                    builder.setIcon(R.drawable.logo);
+                    builder.setMessage("لطفا برنامه را به روزرسانی کنید").setCancelable(false);
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                        }
+                    });
+                    alert = builder.create();
+                    alert.show();
+                }
+            }
+        };
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("error", "error");
+            }
+        };
+        StringRequest request = new StringRequest(Request.Method.GET, url, listener, errorListener);
+        AppController.getInstance().addToRequestQueue(request);
+    }
+
+    public void start() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 startActivity(new Intent(SplashActivity.this, HomeActivity.class));
-                finish();
             }
-        }, 2000);
-
-
+        }, 1000);
     }
 }
